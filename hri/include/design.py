c1e831fb306456ff70aeabc2f2c44c0698681ec8
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-02-23 16:42:50 +0000

    GUI modified: new slider, new lcd

--- hri/include/design.py
+++ hri/include/design.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '../include/mainwindow.ui'
 #
-# Created: Tue Feb 23 11:36:33 2016
+# Created: Tue Feb 23 14:35:37 2016
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
