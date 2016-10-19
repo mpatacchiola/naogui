6c51430e52ca3bec6302c7c110b8440b263d9d32
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-10-19 11:00:24 +0100

    lincense added to main files header

--- hri/src/main.py
+++ hri/src/main.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
+
+#Copyright (c) 2016 Massimiliano Patacchiola
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 ## @package NAO
-#
-# Massimiliano Patacchiola, Plymouth University 2016
-#
 # Scree resolution 1920x1080
 # It is necessary to have pyQt4 installed
 # To generate python code from the .ui file: pyuic4 mainwindow.ui -o design.py
-#
 
 # Libraries for Qt
 from PyQt4 import QtGui  # Import the PyQt4 module we'll need
